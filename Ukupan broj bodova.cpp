8db5203a9d3801dd0d26aea77b48ac614d72893a
cipro <82876139+dromb@users.noreply.github.com>
2022-03-31 07:32:22 +0200

    Add files via upload

---
+++ Ukupan broj bodova.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int sum=0, i=10, n;
+	
+	while(i)
+	{
+		scanf("%d", &n);
+		sum+=n;
+		i--;
+	}
+	printf("Ukupno %d", sum);
+	printf("Prosjecno %f\n", (float)sum/10);
+}
